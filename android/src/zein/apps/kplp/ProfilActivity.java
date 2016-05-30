699e26a7fa752ffac4bfd60c941adc636bf59dea
Ahmad Zein <zein@jayantara.co.id>
2016-05-30 09:01:53 +0700

    update

--- android/src/zein/apps/kplp/ProfilActivity.java
+++ android/src/zein/apps/kplp/ProfilActivity.java
@@ -59,6 +59,7 @@ import android.accounts.Account;
 import android.accounts.AccountManager;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
@@ -93,7 +94,7 @@ import android.widget.Toast;
 
 public class ProfilActivity extends Activity {
 	private static final String TAG = ProfilActivity.class.getSimpleName();
-	private ImageView iv;
+//	private ImageView iv;
    private Matrix matrix = new Matrix();
    private float scale = 1f;
    private ScaleGestureDetector SGD;
@@ -101,11 +102,76 @@ public class ProfilActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_profil);
-		
 
-	      iv=(ImageView)findViewById(R.id.imageView1);
-	      SGD = new ScaleGestureDetector(this,new ScaleListener());
+		TextView text1 = (TextView)findViewById(R.id.textView1);
+		TextView text2 = (TextView)findViewById(R.id.textView2);
+		TextView text3 = (TextView)findViewById(R.id.textView3);
+		TextView text4 = (TextView)findViewById(R.id.textView4);
+		Button btn_kpal_1 = (Button)findViewById(R.id.btn_kapal_1);
+		Button btn_kpal_2 = (Button)findViewById(R.id.btn_kapal_2);
+		Button btn_kpal_3 = (Button)findViewById(R.id.btn_kapal_3);
+		Button btn_kpal_4 = (Button)findViewById(R.id.btn_kapal_4);
 		
+		btn_kpal_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showimage(R.drawable.armada1);
+            }
+        });
+		
+		btn_kpal_2.setOnClickListener(new View.OnClickListener() {
+			  
+            @Override
+            public void onClick(View v) {
+                showimage(R.drawable.armada2);
+            }
+        });
+		
+		btn_kpal_3.setOnClickListener(new View.OnClickListener() {
+			  
+            @Override
+            public void onClick(View v) {
+                showimage(R.drawable.armada3);
+            }
+        });
+		
+		btn_kpal_4.setOnClickListener(new View.OnClickListener() {
+			  
+            @Override
+            public void onClick(View v) {
+                showimage(R.drawable.armada4);
+            }
+        });
+		
+		
+		text1.setText("MAKSUD DAN TUJUAN\n" +
+				"\nProfil Pangkalan PLP tanjung Uban ini disusun dengan maksud sebagai paparan  komprehensif Pangkalan Penjagaan Laut dan Pantai Kelas II Tanjung Uban mengenai tugas  dan fungsinya, sarana prasarana dan capaian hasil serta keberhasilan pelaksanaan tugas  dari Pangkalan PLP Tanjung Uban. Profil ini dibuat dengan tujuan lebih memperkenalkan  Penjagaan Laut dan Pantai Tanjung Uban sebagai bagian dari Kementerian Perhubungan  yang memiliki peran aktif dalam menjaga stabilitas keamanan laut serta pantai maritim  Indonesia." +
+				"\n");
+	
+		text2.setText("TUGAS POKOK DAN FUNGSI (TUPOKSI)\n" +
+				"\nSesuai pasal 3 Keputusasn Menteri Perhubungan Nomor KM.65 Tahun 2002, Disebutkan  bahwa Pangkalan Penjagaan Laut dan Pantai Kelas II Tg. Uban mempunyai fungsi sebagai  berikut :  " +
+				"\n1.	Penyusunan rencana, program dan evaluasi" +
+				"\n2.	Pelaksanaan operasi dan penegakan peraturan dibidang pelayaran" +
+				"\n3.	Pelaksanaan penyelidikan dan penyidikan tindak pidana pelayaran" +
+				"\n4.	Pelaksanaan pengawasan dan penertiban kegiatan salvage dan pekerjaan bawah air, penyelaman instalasi /eksplorasi dan eksploitasi, bangunan di atas dan dibawah air" +
+				"\n5.	Pemberian bantuan pencarian dan pertolongan musibah di laut dan penanggulangan kebakaran" +
+				"\n6.	Pelaksanaan pengamanan dan pengawasan sarana bantu navigasi pelayaran serta penanggulangan pencemaran" +
+				"\n7.	Pelaksanaan pelatihan pengawakan kapal dan instalasi" +
+				"\n8.	Pelaksanaan pengadaan, pemeliharaan perbaikan dan dukungan logistik serta melaksanakan administrasi dan kerumah tanggaan" +
+				"\n");
+		
+		text3.setText("VISI DAN MISI\n" +
+				"\nVisi: Menciptakan keselamatan Transportasi yang aman dan tertib serta perlindungan  lingkungan Maritim diseluruh wilayah perairan laut dan pantai Indonesia    " +
+				"\n\nMisi:  Melakukan pengawasan transportasi laut yang handal Sesuai standar nasional maupun  internasional dengan meningkatkan keselamatan, pengamanan dan ketertiban serta  menegakkan hukum dibidang pelayaran diseluruh wilayah erairan laut dan pantai Indonesia" +
+				"\n");
+		
+		text4.setText("BATAS-BATAS WILAYAH KERJA ARMADA PLP WILAYAH BARAT KPLP TANJUNG UBAN\n");
+
+		
+		
+//	      iv=(ImageView)findViewById(R.id.imageView1);
+//	      SGD = new ScaleGestureDetector(this,new ScaleListener());
+//		
 		
 //		this.webview = (WebView) findViewById(R.id.webView1);
 //
@@ -149,59 +215,17 @@ public class ProfilActivity extends Activity {
 
 	}
 
-	private class ScaleListener extends ScaleGestureDetector.
-	   
-	   SimpleOnScaleGestureListener {
-	      @Override
-	      public boolean onScale(ScaleGestureDetector detector) {
-	         scale *= detector.getScaleFactor();
-	         scale = Math.max(0.1f, Math.min(scale, 5.0f));
-	         
-	         matrix.setScale(scale, scale);
-	         iv.setImageMatrix(matrix);
-	         return true;
-	      }
-	   }
-	
-	public class AppWebViewClients extends WebViewClient {
-	     private ProgressBar progressBar;
-
-	    public AppWebViewClients(ProgressBar progressBar) {
-	        this.progressBar=progressBar;
-	        progressBar.setVisibility(View.VISIBLE);
-	    }
-	    @Override
-	    public boolean shouldOverrideUrlLoading(WebView view, String url) {
-	        // TODO Auto-generated method stub
-	        view.loadUrl(url);
-	        return true;
-	    }
-
-	    @Override
-	    public void onPageFinished(WebView view, String url) {
-	        // TODO Auto-generated method stub
-	        super.onPageFinished(view, url);
-	        progressBar.setVisibility(View.GONE);
-	    }
-	}
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-	
+	Dialog dialog;
+	ImageView ivw;
+public void showimage(int drawable){
+	dialog = new Dialog(ProfilActivity.this,android.R.style.Theme_Black_NoTitleBar_Fullscreen);
+    dialog.setContentView(R.layout.dialog_image_layout);
+    dialog.setCancelable(true);
+    ivw=(ImageView) dialog.findViewById(R.id.goProDialogImage);
+    ivw.setImageResource(drawable);
+ 
+    dialog.show();
+   
+}
 	
 }
